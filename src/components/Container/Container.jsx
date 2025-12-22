66575e975d1245b91df3c887a8def485515a5ab2
GuilhermePacola <gui.pfaccio@gmail.com>
2025-12-22 17:51:49 -0300

    correçao imagem footer

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -3,7 +3,7 @@ import { twMerge } from 'tailwind-merge';
 
 function Container({ className, children, ...props }) {
 
-  const newClassName = twMerge(`max-w-5xl mx-auto py-2 px-4 lg:px-0`, className)
+  const newClassName = twMerge(`max-w-7xl mx-auto py-2 px-4 lg:px-0`, className)
   return (
 
     <div className={newClassName} {...props}>{children}</div>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,12 @@
 import Container from "../Container/Container"
 function Footer(){
     return(
-        <footer className="bg-gray-50 ">
+        <footer className="bg-gray-50">
             <Container className={`flex flex-col md:flex-row items-center gap-4 justify-evenly `}>
                 <p>
                     @2025 E-Commerce Solutions. All rights reserved
                 </p>
-                <img src="../../assets/payment-footer.png" alt="" className={`w-3xs object-cover`}   />
+                <img src="/public/payment-footer.png" alt="" className={`w-3xs object-cover`}   />
             </Container>
         </footer>
     )
