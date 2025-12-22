eab06301b77862277b8d07b6bddcf815b0300788
GuilhermePacola <gui.pfaccio@gmail.com>
2025-12-22 17:43:43 -0300

    correçao imagem footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,12 @@
 import Container from "../Container/Container"
 function Footer(){
     return(
-        <footer className="bg-gray-50">
-            <Container className={` flex py-2 flex-col md:flex-row items-center gap-4 justify-evenly  `}>
+        <footer className="bg-gray-50 ">
+            <Container className={`flex flex-col md:flex-row items-center gap-4 justify-evenly `}>
                 <p>
                     @2025 E-Commerce Solutions. All rights reserved
                 </p>
-                <img src="./src/assets/payment-footer.png" alt="" className={`w-3xs object-cover`}   />
+                <img src="../../assets/payment-footer.png" alt="" className={`w-3xs object-cover`}   />
             </Container>
         </footer>
     )
