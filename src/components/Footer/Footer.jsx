f56df9dea8440e1f530e93c9b61e65c170ccd553
GuilhermePacola <gui.pfaccio@gmail.com>
2025-12-22 16:52:15 -0300

    correçao footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ function Footer(){
                 <p>
                     @2025 E-Commerce Solutions. All rights reserved
                 </p>
-                <img src="../../../public/payment-Footer.png" alt="" className={`w-3xs object-cover`}   />
+                <img src="./src/assets/payment-footer.png" alt="" className={`w-3xs object-cover`}   />
             </Container>
         </footer>
     )
