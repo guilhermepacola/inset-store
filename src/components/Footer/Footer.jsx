078fd5bcc9e704e250871cc8ee9154eaa8e5bed7
GuilhermePacola <gui.pfaccio@gmail.com>
2025-12-23 11:45:46 -0300

    correçao footer viewport

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,12 @@
 import Container from "../Container/Container"
-function Footer(){
-    return(
-        <footer className="bg-gray-50 pb-15 md:pb-5">
+function Footer() {
+    return (
+        <footer className="g-gray-50 border-t border-gray-100 py-4 pb-[calc(env(safe-area-inset-bottom)+1rem)] md:py-6">
             <Container className={`flex flex-col md:flex-row items-center gap-4 justify-evenly  `}>
                 <p>
                     @2025 E-Commerce Solutions. All rights reserved
                 </p>
-                <img src="./payment-footer.png" alt="" className={`w-3xs object-cover`}   />
+                <img src="./payment-footer.png" alt="" className={`w-3xs object-cover`} />
             </Container>
         </footer>
     )
