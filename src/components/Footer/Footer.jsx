55c583b8cf1dc5d7068bb405e8043cd95da946e8
GuilhermePacola <gui.pfaccio@gmail.com>
2025-12-23 11:16:04 -0300

    correçao footer mobile

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,8 @@
 import Container from "../Container/Container"
 function Footer(){
     return(
-        <footer className="bg-gray-50">
-            <Container className={`flex flex-col md:flex-row items-center gap-4 justify-evenly `}>
+        <footer className="bg-gray-50 ">
+            <Container className={`flex flex-col md:flex-row items-center gap-4 justify-evenly mb-5 `}>
                 <p>
                     @2025 E-Commerce Solutions. All rights reserved
                 </p>
